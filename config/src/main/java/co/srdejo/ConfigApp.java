b2601452841ae3b15131748ca154f96e5b41d334
Daniel Jimenez <danielovallos@wposs.com>
2021-02-16 10:35:35 -0500

    Creacion micro

---
+++ config/src/main/java/co/srdejo/ConfigApp.java
@@ -0,0 +1,12 @@
+package co.srdejo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApp.class, args);
+    }
+}
