1620b4ef06e8f3728399195457d6eb45a24f1644
Daniel Jimenez <danielovallos@wposs.com>
2021-03-26 17:21:18 -0500

    creacion inicial de micros

---
+++ micro-producto/src/main/java/co/srdejo/microproducto/ProductoApplication.java
@@ -0,0 +1,13 @@
+package co.srdejo.microproducto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductoApplication.class, args);
+	}
+
+}
---
+++ micro-venta/src/main/java/co/srdejo/microventa/MicroVentaApplication.java
@@ -0,0 +1,13 @@
+package co.srdejo.microventa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroVentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroVentaApplication.class, args);
+	}
+
+}
