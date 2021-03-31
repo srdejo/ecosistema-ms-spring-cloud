dcf7a1a031551f6268a432e5b2515a898d49b9db
Daniel Jimenez <danielovallos@wposs.com>
2021-03-31 12:33:15 -0500

    configuracion db

--- micro-producto/src/main/java/co/srdejo/microproducto/negocio/servicio/ProductoService.java
+++ micro-producto/src/main/java/co/srdejo/microproducto/negocio/servicio/ProductoService.java
@@ -1,10 +1,13 @@
 package co.srdejo.microproducto.negocio.servicio;
 
+import co.srdejo.microproducto.modelo.ProductoEntity;
 import co.srdejo.microproducto.modelo.dto.Producto;
 import co.srdejo.microproducto.negocio.repository.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductoService {
@@ -13,6 +16,9 @@ public class ProductoService {
     private ProductoRepository productoRepository;
 
     public ResponseEntity<Producto> findAll(){
+        List<ProductoEntity> entities = productoRepository.findAll();
+
+
         return (ResponseEntity<Producto>) productoRepository.findAll();
     };
 }
